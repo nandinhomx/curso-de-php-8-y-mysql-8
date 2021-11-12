a6181f78e4d9e42a53182e9f31123fe483353cd5
nandinhomx <ferchonet@gmail.com>
2021-11-12 15:01:43 -0500

    Update BaseDatos.php

--- www/015/lib/BaseDatos.php
+++ www/015/lib/BaseDatos.php
@@ -9,6 +9,14 @@ class BaseDatos {
   private mixed $resultado;
   
   function __construct(
+   /* **************************************************  ME DA ERROR EN ESTA LINEA 
+    [12-Nov-2021 17:40:27 UTC] PHP Fatal error:  Uncaught Error: Typed property Lib\BaseDatos::$servidor must not be accessed before initialization in /home/u689692510/domains/inventario.link/public_html/precios_materiales_0.1/Lib/BaseDatos.php:26
+Stack trace:
+#0 /home/u689692510/domains/inventario.link/public_html/precios_materiales_0.1/Lib/BaseDatos.php(21): Lib\BaseDatos->conectar()
+#1 /home/u689692510/domains/inventario.link/public_html/precios_materiales_0.1/pruebaConexion.php(15): Lib\BaseDatos->__construct()
+#2 {main}
+  thrown in /home/u689692510/domains/inventario.link/public_html/precios_materiales_0.1/Lib/BaseDatos.php on line 26
+    */
     private string $servidor = SERVIDOR,
     private string $usuario = USUARIO,
     private string $pass = PASS,
